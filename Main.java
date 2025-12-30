953fb88fdb5779f79fc421df9df7e71eb8421f1e
Eric Lottsfeldt <elottsfeldt@gmail.com>
2025-12-29 16:56:47 -0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+//Introduction to classes: Multiple fields
+public class Main{
+  public static void main(String[] args) {    
+      Store cookieShop = new Store("cookies", 12, 3.75);
+      System.out.println(cookieShop.productType);
+      System.out.println(cookieShop.inventoryCount);
+      System.out.println(cookieShop.inventoryPrice);
+  }
+}
---
+++ Store.java
@@ -0,0 +1,26 @@
+//Introduction to classes: Multiple fields
+public class Store {
+  // instance fields
+  public String productType;
+  public int inventoryCount;
+  public double inventoryPrice;
+  
+  // constructor method
+  public Store(String product, int count, double price) {
+    productType = product;
+    inventoryCount = count;
+    inventoryPrice = price;
+  }
+
+  public String getProductType() {
+    return productType;
+  }
+
+   public int getInventoryCount() {
+    return inventoryCount;
+  }
+
+  public double getInventoryPrice() {
+    return inventoryPrice;
+  }
+}
